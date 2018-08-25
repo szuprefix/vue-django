9fdee66928abf8b6a68fd5cdc33ea4f522552f84
denishuang <szuprefix@126.com>
2018-08-25 10:51:18 +0800

    array equals delete ";" , remove some unused mobile widget

--- src/utils/array_equals.js
+++ src/utils/array_equals.js
@@ -3,30 +3,30 @@
  */
 // Warn if overriding existing method
 if (Array.prototype.equals)
-  console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+  console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.")
 // attach the .equals method to Array's prototype to call it on any array
 Array.prototype.equals = function (array) {
   // if the other array is a falsy value, return
   if (!array)
-    return false;
+    return false
 
   // compare lengths - can save a lot of time
-  if (this.length != array.length)
-    return false;
+  if (this.length !== array.length)
+    return false
 
   for (var i = 0, l = this.length; i < l; i++) {
     // Check if we have nested arrays
     if (this[i] instanceof Array && array[i] instanceof Array) {
       // recurse into the nested arrays
       if (!this[i].equals(array[i]))
-        return false;
+        return false
     }
-    else if (this[i] != array[i]) {
+    else if (this[i] !== array[i]) {
       // Warning - two different object instances will never be equal: {x:20} != {x:20}
-      return false;
+      return false
     }
   }
-  return true;
+  return true
 }
 // Hide method from for-in loops
-Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+Object.defineProperty(Array.prototype, 'equals', {enumerable: false})
