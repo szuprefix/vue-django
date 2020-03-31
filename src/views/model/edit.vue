09c3bd9ec9ac90d817ba33b388b0930d9d30a26b
denishuang <szuprefix@126.com>
2020-03-31 17:43:37 +0800

    form tabs

--- src/views/model/edit.vue
+++ src/views/model/edit.vue
@@ -1,8 +1,14 @@
 <template>
-    <model-form :appModel="appModel" v-model="data"  ref="form">
+    <model-form :appModel="appModel" v-model="data" ref="form">
         <template v-slot:bottom="{model}">
             <template v-if="model.viewsConfig">
-                <component :is="p.component" :parent="model" v-for="p in model.viewsConfig.pannels" :key="p.name"></component>
+                <el-collapse v-model="activePanels" v-if="model.viewsConfig.pannels">
+                    <el-collapse-item :title="p.label" :name="i" v-for="p, i in model.viewsConfig.pannels"
+                                      :key="p.name">
+                        <component :is="p.component" :parent="model"></component>
+                    </el-collapse-item>
+                </el-collapse>
+
             </template>
             <model-relations v-if="model.data.id && model.viewsConfig"
                              :parent="model"></model-relations>
@@ -16,7 +22,8 @@
         mixins: [edit_mixin],
         data () {
             return {
-                appModel: ''
+                appModel: '',
+                activePanels: [0]
             }
         },
         created () {
