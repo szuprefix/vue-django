0478992eda5f8fbaab52fd5938a6abb856940449
denishuang <szuprefix@126.com>
2021-07-29 18:04:26 +0800

    homepage

--- src/components/stats/MeasureGrid.vue
+++ src/components/stats/MeasureGrid.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-row>
+    <el-row :gutter="10">
         <el-col :xs="24" :sm="12" :md="8" :lg="6" :xl="4" v-for="m, i in value" :key="i">
             <measure :name="m.name" :number="m.number" :icon="m.icon" :iconColor="m.iconColor"
                      :filter="m.filter"></measure>
